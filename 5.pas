72ccec7f19b1fbf07cb580085044531f1ff89b3c
Sin-Dzin-Pin <georg.ponomarev2008@yandex.ru>
2025-09-18 12:18:43 +0300

    Add files via upload

---
+++ 5.pas
@@ -0,0 +1,16 @@
+﻿Program lw125;
+var
+  n,m,k,q:Integer;
+  begin
+    writeln('Введите количесвто банок меда: ');
+    read(n);
+    writeln('Введите объем одной банки меда: ');
+    read(m);
+    writeln('Сколько литров меда нужно медведю? ');
+    read(k);
+    
+    q:=n*m;
+    
+    if q>= k then writeln('YES')
+    ELSE writeln('NO');
+  end.
