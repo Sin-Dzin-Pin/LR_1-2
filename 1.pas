d5faa837ddbf1d06897f916bcc75b04bb6c2c7fa
Sin-Dzin-Pin <georg.ponomarev2008@yandex.ru>
2025-09-18 11:15:52 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,10 @@
+﻿Program lw12;
+var
+a,b:double;
+begin
+  writeln('Введите длину радиуса: ');
+  read(a);
+  b:=Pi*sqr(a);
+  
+  writeln('Площадь круга по радиусу равна: ',round(b,3));
+end.
---
+++ 1.py
@@ -0,0 +1,5 @@
+a=int(input("Введите длину радиуса: "))
+import math
+pi=math.pi
+b=pi*math.pow(a,2)
+print('Площадь круга: ',round(b,3))
