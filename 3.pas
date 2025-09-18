bf13a8e3fe4e6f19268a976e0c681adeed73db04
Sin-Dzin-Pin <georg.ponomarev2008@yandex.ru>
2025-09-18 11:51:41 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,10 @@
+﻿program lw123;
+var
+  a,b:integer;
+  begin
+    writeln('Введите два действительных числа: ');
+    readln(a,b);
+    
+    if a>b then writeln(a)
+    else writeln(a,' ',b);
+  end.
